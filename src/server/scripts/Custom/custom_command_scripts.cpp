1a8fa1844c39dd84f49eb10ef58cf09dddaf48e9
dooman <emuldong@gmail.com>
2011-10-25 23:40:42 -0500

    Custom command script for .buff extended command

---
+++ src/server/scripts/Custom/custom_command_scripts.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ScriptPCH.h"
+#include "MapManager.h"
+#include "Chat.h"
+#include "ObjectMgr.h"
+#include "AccountMgr.h"
+
+class custom_commandscript : public CommandScript
+{
+public:
+	custom_commandscript() : CommandScript("misc_commandscript") { }
+
+	ChatCommand* GetCommands() const
+	{
+		static ChatCommand buffCommandTable[] =
+		{
+				{ "name",           SEC_GAMEMASTER,      false, &HandleBuffNameCommand,            "", NULL },
+				{ "all",			SEC_GAMEMASTER,      false, &HandleBuffAllCommand,             "", NULL },
+				{ "",               SEC_GAMEMASTER,      false, NULL,                              "", NULL },
+				{ NULL,             0,                  false, NULL,                              "", NULL }
+		};
+		static ChatCommand commandTable[] =
+		{
+				{ "buff",           SEC_GAMEMASTER,      false, NULL,                   "", buffCommandTable },
+				{ NULL,             0,                  false,  NULL,                       "", NULL }
+		};
+		return commandTable;
+	}
+
+	// buff player with given game_buff.entry
+	static bool HandleBuffNameCommand(ChatHandler* handler, const char* args)
+	{
+		if (!*args)
+			return false;
+
+		char* nameStr = strtok((char*)args, " ");
+		Player* target;
+		uint64 target_guid;
+		std::string target_name;
+
+		if (!handler->extractPlayerTarget(nameStr, &target, &target_guid, &target_name))
+			return false;
+
+		if (target)
+		{
+			if (handler->HasLowerSecurity(target, 0))
+				return false;
+
+			std::string chrNameLink = handler->playerLink(target_name);
+			target->CastSpell(target, 24425, false);
+		}
+		else
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	// buff all online players
+	static bool HandleBuffAllCommand(ChatHandler* handler, const char* /*args*/)
+	{
+		Player* target;
+		uint64 target_guid;
+		std::string target_name;
+
+		QueryResult resultDB = CharacterDatabase.Query("SELECT name FROM characters WHERE online > 0");
+		if (!resultDB)
+		{
+			handler->SendSysMessage(LANG_ACCOUNT_LIST_EMPTY);
+			return true;
+		}
+
+
+		do
+		{
+			Field* fieldsDB = resultDB->Fetch();
+			std::string str = fieldsDB[0].GetString();
+			std::vector<char> writable(str.begin(), str.end());
+			writable.push_back('\0');
+			char* nameStr = &writable[0];
+
+
+			if (!handler->extractPlayerTarget(nameStr, &target, &target_guid, &target_name))
+				return false;
+			if (handler->HasLowerSecurity(target,0))
+				continue;
+
+			target->CastSpell(target, 24425, false);
+		}while (resultDB->NextRow());
+
+
+
+		return true;
+	}
+
+};
+
+void AddSC_custom_commandscript()
+{
+	new custom_commandscript();
+}

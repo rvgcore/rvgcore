a0beca247bc0818f24e27908263c232e079b0c82
Edward Muldong <emuldong@gmail.com>
2011-11-14 20:51:04 -0600

    Restructure Folders

--- sql/Customs/custom_command_sakura.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `command` WHERE `name` = 'sakura';
-
-INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('sakura', 3, 'Syntax: .sakura $dir #value\r\n\r\nTeleport in the direction indicated by dir (f=forward,b=backward,u=up,d=down) by the number of coordinate units specified as value.');
